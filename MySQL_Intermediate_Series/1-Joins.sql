b9fd7c40f5ba0dc2b892a2905f7709fbf12cd2e1
grayMafia <khorshed1921@gmail.com>
2025-09-06 00:32:43 +0600

    Learned Left, Right and Self joins

--- MySQL_Intermediate_Series/1-Joins.sql
+++ MySQL_Intermediate_Series/1-Joins.sql
@@ -23,3 +23,40 @@ FROM employee_demographics AS dem
 INNER JOIN employee_salary AS sal
 	ON dem.employee_id = sal.employee_id
 ;
+
+-- Outer Joins (Left and Right)
+
+-- Left Join (Takes everything from left table)
+
+SELECT *
+FROM employee_demographics AS dem
+LEFT JOIN employee_salary AS sal
+	ON dem.employee_id = sal.employee_id
+;
+
+-- Right Join (Takes everything from right table)
+
+SELECT *
+FROM employee_demographics AS dem
+RIGHT JOIN employee_salary AS sal
+	ON dem.employee_id = sal.employee_id
+;
+
+-- Self Join
+
+SELECT *
+FROM employee_salary AS emp1
+JOIN employee_salary AS emp2
+	ON emp1.employee_id + 1 = emp2.employee_id
+;
+
+SELECT emp1.employee_id AS emp_santa,
+emp1.first_name AS first_name_santa,
+emp1.last_name AS last_name_santa,
+emp2.employee_id AS emp_id,
+emp2.first_name AS first_name_emp,
+emp2.last_name AS last_name_emp
+FROM employee_salary AS emp1
+JOIN employee_salary AS emp2
+	ON emp1.employee_id + 1 = emp2.employee_id
+;
