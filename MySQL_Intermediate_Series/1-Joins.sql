3945d5ac46a1b033707a0751305b264971cd0f36
grayMafia <khorshed1921@gmail.com>
2025-09-06 00:38:27 +0600

    Learned joining multiple tables

--- MySQL_Intermediate_Series/1-Joins.sql
+++ MySQL_Intermediate_Series/1-Joins.sql
@@ -60,3 +60,22 @@ FROM employee_salary AS emp1
 JOIN employee_salary AS emp2
 	ON emp1.employee_id + 1 = emp2.employee_id
 ;
+
+-- Joining multiple tables
+
+SELECT *
+FROM employee_demographics AS dem
+INNER JOIN employee_salary AS sal
+	ON dem.employee_id = sal.employee_id
+;
+
+SELECT *
+FROM parks_departments;
+
+SELECT *
+FROM employee_demographics AS dem
+INNER JOIN employee_salary AS sal
+	ON dem.employee_id = sal.employee_id
+INNER JOIN parks_departments AS pd
+	ON sal.dept_id = pd.department_id
+;
