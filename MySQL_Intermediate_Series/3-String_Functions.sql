e5897a1c91ac6528fade06068c2fe150f9f6d1ae
grayMafia <khorshed1921@gmail.com>
2025-09-07 23:50:50 +0600

    Learned Length, Upper, Lower, Trim, Substring

---
+++ MySQL_Intermediate_Series/3-String_Functions.sql
@@ -0,0 +1,54 @@
+-- String Functions
+
+-- Length
+
+SELECT first_name, LENGTH(first_name)
+FROM employee_demographics
+ORDER BY 2
+;
+
+-- Upper
+
+SELECT first_name, UPPER(first_name)
+FROM employee_demographics
+;
+
+-- Lower
+
+SELECT first_name, LOWER(first_name)
+FROM employee_demographics
+;
+
+-- Trim
+
+SELECT TRIM('      Hello     ');
+
+-- Left Trim
+SELECT LTRIM('      Hello      ');
+
+-- Right Trim
+SELECT RTRIM('       Hello     ');
+
+-- Substring
+
+SELECT first_name, LEFT(first_name, 4)
+FROM employee_demographics
+;
+
+SELECT first_name,
+LEFT(first_name, 4),
+RIGHT(first_name, 3)
+FROM employee_demographics
+;
+
+SELECT first_name,
+LEFT(first_name, 4),
+RIGHT(first_name, 3), 
+SUBSTRING(first_name, 3, 2)
+FROM employee_demographics
+;
+
+SELECT birth_date,
+SUBSTRING(birth_date, 6, 2) AS birth_month
+FROM employee_demographics
+;
