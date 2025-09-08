78f849cb3ec8b8884accf0f3846744bc54eea9ef
grayMafia <khorshed1921@gmail.com>
2025-09-08 15:32:48 +0600

    Learned window

---
+++ MySQL_Intermediate_Series/6-Window.sql
@@ -0,0 +1,30 @@
+-- Window function
+
+SELECT gender, AVG(salary) AS avg_salary
+FROM employee_demographics AS dem
+JOIN employee_salary AS sal
+	ON dem.employee_id = sal.employee_id
+GROUP BY gender
+;
+
+SELECT dem.first_name, dem.last_name, gender, AVG(salary) OVER(PARTITION BY gender)
+FROM employee_demographics AS dem
+JOIN employee_salary AS sal
+	ON dem.employee_id = sal.employee_id
+;
+
+SELECT dem.employee_id, dem.first_name, dem.last_name, gender, sal.salary, SUM(salary) OVER(PARTITION BY gender ORDER BY dem.employee_id) AS rolling_total
+FROM employee_demographics AS dem
+JOIN employee_salary AS sal
+	ON dem.employee_id = sal.employee_id
+;
+
+
+SELECT dem.employee_id, dem.first_name, dem.last_name, gender, salary,
+	ROW_NUMBER() OVER(PARTITION BY gender ORDER BY salary DESC) AS row_num,
+    RANK() OVER(PARTITION BY gender ORDER BY salary DESC) AS rank_num,
+    DENSE_RANK() OVER(PARTITION BY gender ORDER BY salary DESC) AS rank_num
+FROM employee_demographics AS dem
+JOIN employee_salary AS sal
+	ON dem.employee_id = sal.employee_id
+;
